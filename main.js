ba2f0aa8f64bd7c933f41d0155e98eef888c747e
WebsitesPages <129342147+WebsitesPages@users.noreply.github.com>
2023-03-30 02:21:10 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,37 +2,47 @@ document.addEventListener('DOMContentLoaded', function() {
     const heroVideo = document.getElementById('heroVideo');
     const playbackSpeed = parseFloat(heroVideo.dataset.speed);
     heroVideo.playbackRate = playbackSpeed;
-});
 
-document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
-
-    window.addEventListener("scroll", function () {
-        if (window.pageYOffset > 50) {
-            header.style.top = "0"; // Zeigt den Header an, wenn man nach unten scrollt
-        } else {
-            header.style.top = "-100px"; // Versteckt den Header, wenn man oben angekommen ist
-        }
-    });
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const heroVideo = document.getElementById("heroVideo");
 
     function playVideoOnLoad() {
         heroVideo.play();
     }
 
     window.addEventListener("load", playVideoOnLoad);
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const heroVideo = document.getElementById("heroVideo");
 
-    function playVideoOnInteraction() {
-        heroVideo.play();
-        document.removeEventListener("scroll", playVideoOnInteraction);
-        document.removeEventListener("touchstart", playVideoOnInteraction);
-    }
 
-    document.addEventListener("scroll", playVideoOnInteraction);
-    document.addEventListener("touchstart", playVideoOnInteraction);
+    
+    window.addEventListener("scroll", function(){
+
+    const html = document.documentElement;
+    let scrollPosition = window.pageYOffset;
+    let banner1 = document.querySelector(".hero")
+
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop/maxScrollTop;
+    const banner_content_h2 = document.querySelector(".banner-content");
+    const halfwayCounted = 1-(scrollFraction*8);
+    console.log(halfwayCounted);
+    const banner_content_p = document.querySelector(".banner_content_p");
+    const cta = document.querySelector(".cta");
+
+        if(halfwayCounted <= -3.7){
+            banner_content_h2.style.opacity = "1";
+            banner_content_h2.style.transition = "2s";
+            
+        }else{
+            banner_content_h2.style.opacity = "0";
+            
+        }
+
+if (window.pageYOffset > 50) {
+            header.style.top = "0"; // Zeigt den Header an, wenn man nach unten scrollt
+        } else {
+            header.style.top = "-100px"; // Versteckt den Header, wenn man oben angekommen ist
+        }
+
+})
+
 });
