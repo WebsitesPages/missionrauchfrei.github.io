fd1710d3f47ac067c2f9c20a821f896166c44d4c
WebsitesPages <129342147+WebsitesPages@users.noreply.github.com>
2023-05-27 20:23:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,164 +1,153 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const heroVideo = document.getElementById('heroVideo');
-    const playbackSpeed = parseFloat(heroVideo.dataset.speed);
-    heroVideo.playbackRate = playbackSpeed;
 
-    const header = document.querySelector("header");
+  const html = document.documentElement;
+  let scrollPosition = window.pageYOffset;
+  let banner1 = document.querySelector(".hero");
 
-    function playVideoOnLoad() {
-        heroVideo.play();
-    }
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop/maxScrollTop;
+  let halfwayCounted = 1-(scrollFraction*8);
+  halfwayCounted = halfwayCounted.toFixed(1);
+  console.log(halfwayCounted);
 
-    window.addEventListener("load", playVideoOnLoad);
+  const banner_content_h2 = document.querySelector(".banner-content");
+  const banner_content_p = document.querySelector(".banner_content_p");
+  const cta = document.querySelector(".cta");
 
-    const html = document.documentElement;
-    let scrollPosition = window.pageYOffset;
-    let banner1 = document.querySelector(".hero");
-
-    const scrollTop = html.scrollTop;
-    const maxScrollTop = html.scrollHeight - window.innerHeight;
-    const scrollFraction = scrollTop/maxScrollTop;
-    let halfwayCounted = 1-(scrollFraction*8);
-    halfwayCounted = halfwayCounted.toFixed(1);
-    console.log(halfwayCounted);
-
-    const banner_content_h2 = document.querySelector(".banner-content");
-    const banner_content_p = document.querySelector(".banner_content_p");
-    const cta = document.querySelector(".cta");
-
-    
-
-    // if (window.pageYOffset > 50) {
-    //     header.style.top = "0"; // Zeigt den Header an, wenn man nach unten scrollt
-    // } else {
-    //     header.style.top = "-100px"; // Versteckt den Header, wenn man oben angekommen ist
-    // }
-
-    const arrow = document.querySelector(".c5");
-    const hero_content = document.querySelector(".hero-content");
-    const arrow_text = document.querySelector(".c6");
-    
-    if (halfwayCounted = 1){
-        hero_content.style.opacity = "1";
-        hero_content.style.transition = "2.5s";
-    }
+  const hero_content = document.querySelector(".hero-content");
   
-    if (halfwayCounted <= 0) {
-        arrow.style.opacity = halfwayCounted;
-        arrow.style.transition = ".5s";
-        arrow_text.style.opacity = halfwayCounted;
-        arrow_text.style.transition = ".5s";
-    } else {
-        arrow.style.opacity = "1";
-    }
+  if (halfwayCounted = 1){
+      hero_content.style.opacity = "1";
+      hero_content.style.transition = "2.5s";
+  }
 
-    window.addEventListener("scroll", function(){
-        let scrollPosition = window.pageYOffset;
+  window.addEventListener("scroll", function(){
+      let scrollPosition = window.pageYOffset;
 
-        const scrollTop = html.scrollTop;
-        const maxScrollTop = html.scrollHeight - window.innerHeight;
-        const scrollFraction = scrollTop/maxScrollTop;
-        halfwayCounted = 1-(scrollFraction*8);
-        halfwayCounted = halfwayCounted.toFixed(1);
-        console.log(halfwayCounted);
+      const scrollTop = html.scrollTop;
+      const maxScrollTop = html.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop/maxScrollTop;
+      halfwayCounted = 1-(scrollFraction*8);
+      halfwayCounted = halfwayCounted.toFixed(1);
+      console.log(halfwayCounted);
 
-  const h3Elements = document.querySelectorAll(".banner_content_h3");
-  let delay = 0;
+            
+  });
 
-  h3Elements.forEach((h3) => {
-    h3.classList.add("hidden");
+  //Loading Screen
+  function removePreloader() {
+    const preloader = document.getElementById('preloader');
+  
     setTimeout(() => {
-      h3.classList.remove("hidden");
-      h3.style.transition = "opacity 1s";
-      h3.style.opacity = 1;
-    }, delay);
-    delay += 1000; // Zeit in Millisekunden, hier 1000 ms = 1 Sekunde
-  });
+      preloader.style.transition = 'opacity 0.5s';
+      preloader.style.opacity = '0';
+    }, 500);
+  
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 1000);
+  }
+  window.addEventListener('load', removePreloader);
 
-   const banner2 = document.querySelector(".banner2");
+  const heroTitle = document.getElementById('hero-title');
+  const heroSubtitle = document.getElementById('hero-subtitle');
 
-//   const observerOptions = {
-//       root: null,
-//       rootMargin: "50px",
-//       threshold: 0.8,
-//   };
+  setTimeout(function() {
+      heroTitle.style.transition = 'opacity 1s';
+      heroTitle.style.opacity = '1';
+  }, 1000);  // Startet die Animation 1 Sekunde nach dem Laden der Seite
 
-//   const handleIntersection = (entries, observer) => {
-//       entries.forEach((entry) => {
-//           if (entry.isIntersecting) {
-              
-//               banner2.style.backgroundColor = "white";
-//           } else {
-//               // Ändern Sie die Farbe zurück, wenn der Bildschirm nicht mehr auf der Klasse 'banner2' ist
-        
-//               banner2.style.backgroundColor = "black";
-//           }
-//       });
-//   };
+  setTimeout(function() {
+      heroSubtitle.style.transition = 'opacity 1s';
+      heroSubtitle.style.opacity = '1';
+  }, 1500);  // Startet die Animation 1 Sekunde nach dem Laden der Seite
+   const banner3Els = document.querySelectorAll('.banner_content_h3');
+    const lastBanner3El = banner3Els[banner3Els.length - 1];
 
-//   const observer = new IntersectionObserver(handleIntersection, observerOptions);
-//   observer.observe(banner2);
+    const banner2El = document.querySelector('.banner2_content_h2');
 
-    // if (halfwayCounted <=-5){
-    //     banner2.style.backgroundColor = "white";
-    // } else {
-    //     banner2.style.backgroundColor = "black";
-    // }
-
-        if (window.pageYOffset > 50) {
-            header.style.top = "0"; // Zeigt den Header an, wenn man nach unten scrollt
-        } else {
-            header.style.top = "-100px"; // Versteckt den Header, wenn man oben angekommen ist
-        }
-
-        if (halfwayCounted <= 0) {
-            arrow.style.opacity = halfwayCounted;
-            arrow.style.transition = ".5s";
-            arrow_text.style.opacity = halfwayCounted;
-            arrow_text.style.transition = ".5s";
-        } else {
-            arrow.style.opacity = "1";
-            arrow_text.style.opacity = "1";
-        }
+    lastBanner3El.addEventListener('transitionend', () => {
+        banner2El.classList.add('show');
     });
-
-
-
-const nav = document.querySelector(".banner2");
-const sectionOne = document.querySelector(".banner");
-const banner2items = document.querySelectorAll(".banner2-item");
-const curve = document.querySelector(".curve");
-
-const sectionOneOptions = {
-  root: null,
-  rootMargin: "-150px",
-  treshold: 0.1
-};
-
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      nav.classList.remove("backgroundblack");
-      curve.classList.remove("opacitynull");
-      banner2items.forEach(item => {
-        item.classList.remove("opacitynull");
-      });
-    } else {
-      nav.classList.add("backgroundblack");
-      curve.classList.add("opacitynull")
-      banner2items.forEach(item => {
-        item.classList.add("opacitynull");
-      });
-    }
-  });
-}, sectionOneOptions);
-
-sectionOneObserver.observe(sectionOne);
-
-
 });
 
+     
+let hr = document.querySelector('.banner2');
+
+const sectionOneOptions = {
+    root: null,
+    rootMargin: '40% 0% -40% 0%',
+    threshold: 0
+};
+
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(`Intersection Ratio: ${entry.intersectionRatio}`);
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+        }
+    });
+}, sectionOneOptions);
+
+observer.observe(hr);
+
+
   
+document.addEventListener('DOMContentLoaded', function() {
+  const banner = document.querySelector('.banner');
+  const h3Elements = banner.querySelectorAll(".banner_content_h3");
+  const downloadButton = banner.querySelector('.download-button');
+
+  const observerOptions = {
+      root: null,
+      rootMargin: '20% 0% -20% 0%',
+      threshold: 0
+  };
+  
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              let delay = 0;
+              h3Elements.forEach((h3) => {
+                  setTimeout(() => {
+                  h3.classList.remove("hidden");
+                  h3.style.transition = "opacity 1s";
+                  h3.style.opacity = 1;
+                  }, delay);
+                  delay += 1000; // Zeit in Millisekunden, hier 1000 ms = 1 Sekunde
+              });
+
+              // Zeige den Download-Button nachdem alle h3-Elemente angezeigt wurden
+              setTimeout(() => {
+                  downloadButton.style.opacity = 1;
+                  downloadButton.style.transition = "opacity 1s";
+              }, delay);
+
+              observer.unobserve(entry.target);
+          }
+      });
+  }, observerOptions);
+
+  observer.observe(banner);
+});
+
+let itembanner2 = document.querySelectorAll('.banner2-item');
+
+itembanner2.forEach(itembanner2 => {
+  let observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          console.log(`Intersection Ratio: ${entry.intersectionRatio}`);
+          if (entry.isIntersecting) {
+              entry.target.style.opacity = "1";
+              entry.target.style.transition = "opacity 1s";
+          }
+      });
+  }, sectionOneOptions);
+
+  observer.observe(itembanner2);
+});
+
+
+
+
